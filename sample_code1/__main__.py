1879de5931a67708b6385959077e4e3eab7fd74e
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2026-06-21 10:46:38 +0700

    init cleaner

--- sample_code1/__main__.py
+++ sample_code1/__main__.py
@@ -1,13 +1,5 @@
 import requests
 
-# Send a GET request
-response = requests.get('https://httpbin.org/get', params={'search': 'python'})
+x = requests.get('https://w3schools.com/python/demopage.htm')
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Print the raw text data
-    print(response.text)
-    
-    # Parse and print response as JSON if applicable
-    data = response.json()
-    print(data)
+print(x.text)
